393c2cb16fb6a99c621cdbe0a21603229455b325
Richard Shen <richarshen@gmail.com>
2015-09-16 20:03:13 -0500

    AMainPhase fixed

--- backend/src/main/java/testGame/AMainPhase.java
+++ backend/src/main/java/testGame/AMainPhase.java
@@ -3,9 +3,9 @@ package testGame;
 import org.json.JSONObject;
 
 import com.three_stack.digital_compass.backend.GameState;
-import com.three_stack.digital_compass.backend.MainPhase;
+import com.three_stack.digital_compass.backend.Phase;
 
-public class AMainPhase extends MainPhase {
+public class AMainPhase extends Phase {
 	
 	public GameState processAction(JSONObject action, GameState state) {
 		return null;
