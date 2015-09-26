4608b4bacb25903136fd5ee3cad53e5da653c9f4
Jason Lin <jlin51@illinois.edu>
2015-09-25 19:40:13 -0500

    .

--- backend/src/main/java/testGame/RevealPhase.java
+++ backend/src/main/java/testGame/RevealPhase.java
@@ -8,8 +8,16 @@ public class RevealPhase extends BasicPhase {
 
 	@Override
 	public BasicGameState processAction(BasicAction action, BasicGameState state) {
+		GameState gameState = (GameState) state;
+        RevealAction revealAction = (RevealAction) action;
+        
 		// TODO Auto-generated method stub
-		return null;
+		if(revealAction.isMoveOn()) {
+			gameState.setCurrentPhase(new ScorePhase());
+		} else {
+			System.out.println("Uh Oh. Received a weird RevealAction");
+		}
+		return gameState;
 	}
 
 }
