00ab28a722eaba3c4643ab46058047ad87862552
Jason Lin <jlin51@illinois.edu>
2015-09-25 19:33:08 -0500

    .

---
+++ backend/src/main/java/testGame/RevealPhase.java
@@ -0,0 +1,15 @@
+package testGame;
+
+import com.three_stack.digital_compass.backend.BasicAction;
+import com.three_stack.digital_compass.backend.BasicGameState;
+import com.three_stack.digital_compass.backend.BasicPhase;
+
+public class RevealPhase extends BasicPhase {
+
+	@Override
+	public BasicGameState processAction(BasicAction action, BasicGameState state) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
