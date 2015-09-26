5c4b9fe5cf941d98e8334b32a5178967b8c2a01e
Jason Lin <jlin51@illinois.edu>
2015-09-25 20:19:32 -0500

    .

---
+++ backend/src/main/java/testGame/Belief.java
@@ -0,0 +1,18 @@
+package testGame;
+
+import java.util.ArrayList;
+
+public abstract class Belief {
+	protected ArrayList<String> believers;
+	
+	public Belief() {
+		believers = new ArrayList<>();
+	}
+	public ArrayList<String> getBelievers() {
+		return believers;
+	}
+
+	public void setBelievers(ArrayList<String> believers) {
+		this.believers = believers;
+	}
+}
