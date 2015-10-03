0c51bea3470949fe8159508c9de3168b237aa81e
Jason Lin <jlin51@illinois.edu>
2015-10-02 23:02:54 -0500

    fixing gamestate bugs

--- backend/src/main/java/testGame/GameStateFactory.java
+++ backend/src/main/java/testGame/GameStateFactory.java
@@ -9,6 +9,8 @@ import com.three_stack.digital_compass.backend.BasicGameStateFactory;
 public class GameStateFactory extends BasicGameStateFactory {
     @Override
     public BasicGameState createState() {
+    	GameState gs = new GameState();
+    	gs.setCurrentPhase(new LiePhase());
         return new GameState();
     }
 }
