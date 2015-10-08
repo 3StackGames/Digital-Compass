bacbf2ad35b4ad50057076ac09ee13bc6bedff2e
Jason Lin <jlin51@illinois.edu>
2015-10-08 01:26:52 -0500

    able to query from mongodb now

--- backend/src/main/java/testGame/LiePhase.java
+++ backend/src/main/java/testGame/LiePhase.java
@@ -1,5 +1,16 @@
 package testGame;
 
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import org.bson.Document;
+
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
+import com.mongodb.client.MongoDatabase;
 import com.three_stack.digital_compass.backend.BasicAction;
 import com.three_stack.digital_compass.backend.BasicGameState;
 import com.three_stack.digital_compass.backend.BasicPhase;
@@ -8,7 +19,6 @@ import com.three_stack.digital_compass.backend.BasicPhase;
  * Created by Hyunbin on 9/16/15.
  */
 public class LiePhase extends BasicPhase {
-
 	public LiePhase(GameState gameState) {
 		super();
 		setAction(LieAction.class);
@@ -19,9 +29,34 @@ public class LiePhase extends BasicPhase {
         // The case where we're on a new question cycle
         // TODO: @Hyunbin: Should pull question randomly from bank
         // TODO: @Hyunbin: Set correct answer in the game state to prevent duplicate right answers
-        Question question = new Question("Who is Casper?", "My Best Friend");
-        gameState.setCurrentQuestion(question);
+        gameState.setCurrentQuestion(getNewQuestion(gameState));
         gameState.incrementQuestionCount();
+    }
+    
+    private Question getNewQuestion(GameState gameState) {
+    	//TODO: Extract this elsewhere to improve performance
+    	//Connect to MongoDB
+    	MongoClient mongoClient = new MongoClient(Config.MONGO_ADDRESS, Config.MONGO_PORT);
+    	MongoDatabase db = mongoClient.getDatabase(Config.MONGO_DATABASE);
+    	MongoCollection<Document> collection = db.getCollection(Config.MONGO_PACK_COLLECTION);
+    	//get all packs
+    	List<Document> packs = new ArrayList<>();
+    	MongoCursor<Document> cursor = collection.find().iterator();
+    	while(cursor.hasNext()) {
+    		packs.add(cursor.next());
+    	}
+    	cursor.close();
+    	mongoClient.close();
+    	//pick a pack
+    	Random random = new Random();
+    	Document chosenPack = packs.get(random.nextInt(packs.size()));
+    	//pick a Question
+    	List<Document> questions = (List<Document>) chosenPack.get("questions");
+    	Document chosenQuestion = questions.get(random.nextInt(questions.size()));
+    	//get a
+    	//Create Question
+    	Question newQuestion = new Question(chosenQuestion.getString("prompt"), chosenQuestion.getString("answer"));
+    	return newQuestion;
     }
 
     public BasicGameState processAction(BasicAction action, BasicGameState state) {
