6398e52426a33c724cbcef7871ef42a66e95faea
Jason Lin <jlin51@illinois.edu>
2015-10-08 16:00:56 -0500

    added config file

---
+++ backend/src/main/java/testGame/Config.java
@@ -0,0 +1,8 @@
+package testGame;
+
+public class Config {
+	public static final String MONGO_ADDRESS = "localhost";
+	public static final int MONGO_PORT = 27017;
+	public static final String MONGO_PACK_COLLECTION = "Packs";
+	public static final String MONGO_DATABASE = "SubtleScheme";
+}
