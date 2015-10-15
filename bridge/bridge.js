921f3b11be8ed28fd09a308b7d1a7d1791a97301
Jason Lin <jlin51@illinois.edu>
2015-10-15 10:17:04 -0700

    cleaned up bridge code. no major features changed. just refactoring

--- bridge/bridge.js
+++ bridge/bridge.js
@@ -3,8 +3,8 @@ var events = require('./events.js');
 var codeGenerator = require('./code-generator.js');
 var Game = require('./game.js');
 var Player = require('./player.js');
-var logging = true;
-
+var logger = require('./logger.js');
+logger.logging = true;
 /*======================
   Global Variables
 =======================*/
@@ -15,10 +15,9 @@ var backendSocket = null;
   Helper Functions
 =======================*/
 var joinGame = function(socket, gameCode, name) {
-  if(logging) console.log('adding gamepad to room: ' + gameCode);
   socket.join(gameCode);
   //add to game
-  if(logging) console.log(games);
+  logger.log(games);
   games[gameCode].players.push(new Player(name));
 }
 
@@ -33,67 +32,56 @@ var createGame = function(socket, gameCode) {
 
 io.on(events.CONNECTION, function(socket) {
   /*===== BACKEND ========*/
-  socket.on(BACKEND_CONNECTED, function() {
-    if(logging) console.log('Backend Connected');
+  socket.on(events.BACKEND_CONNECTED, function() {
     //setup
     backendSocket = socket;
     games = {};
+    logger.log('Backend Connected');
 
     //relay
     socket.on(events.STATE_UPDATE, function(data) {
-      if(logging) {
-        console.log('State Update Received from Backend: ');
-        console.log(data);
-      }
       var parsedData = JSON.parse(data);
       io.to(parsedData.gameCode).emit(events.STATE_UPDATE, parsedData);
-      if(logging) console.log('State Update Relayed to Frontend');
+      logger.log('State Update Relayed from Backend to Frontend', data);
     });
   });
 
   /*===== DISPLAY ========*/
   socket.on(events.DISPLAY_JOIN, function() {
-    if(logging) console.log('Display Joined');
     //setup
     var gameCode = codeGenerator.generate(games);
     socket.gameCode = gameCode;
     createGame(socket, gameCode);
     socket.emit(events.STATE_UPDATE, games[gameCode]);
-    if(logging) console.log('State Update Sent to Display');
+    logger.log('Display Joined. State Update sent to Display.');
 
     //Relay
     socket.on(events.DISPLAY_ACTION_COMPLETE, function() {
-      if(logging) console.log('Display Action Complete Received');
-      //relay to gamepads
       io.to(socket.gameCode).emit(events.DISPLAY_ACTION_COMPLETE);
-      if(logging) console.log('Display Action Relayed to gamepads');
+      logger.log('Display Action Commplete Received and Relayed to everyone');
     });
   });
 
   /*===== GAMEPAD ========*/
   socket.on(events.GAMEPAD_JOIN, function(data) {
-    if(logging) console.log('Gamepad joined');
     var gameCode = data.gameCode;
     //setup
     joinGame(socket, gameCode, data.name);
     socket.gameCode = gameCode;
     //let everyone know a new player has joined
     io.to(gameCode).emit(events.STATE_UPDATE, games[gameCode]);
-    if(logging) console.log('Sent Player Joined to Everyone');
+    logger.log('Gamepad Joined. Update sent to everyone.', games[gameCode]);
 
     //Begin Game
     socket.on(events.BEGIN_GAME, function() {
-      if(logging) console.log('Begin Game Received. Sending Game to Backend: ');
-      if(logging) console.log(games[socket.gameCode]);
       backendSocket.emit(events.INITIALIZE_GAME, games[socket.gameCode]);
-      if(logging) console.log('Sent Initialize Game to backend');
+      logger.log('Begin Game Received. Sent Initialize Game to Backend.', games[socket.gameCode]);
     });
 
     //Relay
     socket.on(events.GAMEPAD_INPUT, function(data) {
-      if(logging) console.log('Gamepad input received');
       backendSocket.emit(events.GAMEPAD_INPUT, data);
-      if(logging) console.log('Gamepad input relayed to backend');
+      logger.log('Gamepad input received and relayed to backend', data);
     });
   });
 });
