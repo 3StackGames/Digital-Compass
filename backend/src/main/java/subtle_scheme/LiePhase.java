13d9910d9689ef46c480e6718329afe9bd438c76
Jason Lin <jlin51@illinois.edu>
2015-10-27 18:30:03 -0500

    Added simple print statement to test disconnecting

--- backend/src/main/java/subtle_scheme/LiePhase.java
+++ backend/src/main/java/subtle_scheme/LiePhase.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
+import com.three_stack.digital_compass.backend.BasicPlayer;
 import org.bson.Document;
 
 import com.mongodb.MongoClient;
@@ -51,6 +52,18 @@ public class LiePhase extends BasicPhase {
 		}
 		return gameState;
 	}
+
+    @Override
+    public BasicGameState onDisconnect(BasicGameState state, BasicPlayer player) {
+        System.out.println("Player Disconnected: " + player.getDisplayName());
+        return super.onDisconnect(state, player);
+    }
+
+    @Override
+    public BasicGameState onReconnect(BasicGameState state, BasicPlayer player) {
+        System.out.println("Player Reconnected: " + player.getDisplayName());
+        return super.onReconnect(state, player);
+    }
 
     private Document getNewQuestion(GameState gameState, MongoCollection<Document> packCollection, MongoCollection<Document> questionCollection) {
         List<Document> allUnaskedQuestions = getAllUnaskedQuestions(gameState, packCollection, questionCollection);
