010b2253476ddc15c66752f3bd52f2de038f4969
Richard Shen <richarshen@gmail.com>
2015-12-01 19:02:16 -0600

    constructor fix

--- backend/src/main/java/com/three_stack/digital_compass/backend/InvalidInputException.java
+++ backend/src/main/java/com/three_stack/digital_compass/backend/InvalidInputException.java
@@ -7,15 +7,24 @@ public class InvalidInputException extends 	Exception {
 	 */
 	private static final long serialVersionUID = -5121994118845234438L;
 	
+	public InvalidInputException() {
+		super();
+	}
+	
+	public InvalidInputException(String message) {
+		super(message);
+	}
+	
+	public InvalidInputException(Code code, String message) {
+		super(message);
+		this.code = code;
+	}
+	
 	public static enum Code {
 		INPUT_REJECTED
 	}
 	
 	private Code code;
-	
-	public void setCode(Code code) {
-		this.code = code;
-	}
 	
 	public Code getCode() {
 		return code;
