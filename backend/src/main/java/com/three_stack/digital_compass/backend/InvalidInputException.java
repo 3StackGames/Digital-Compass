8571647e50104b64e5b3179465b13e06ce4a15db
Richard Shen <richarshen@gmail.com>
2016-01-01 09:51:02 -0600

    added ILLEGAL_STATE code to InvalidInputException

--- backend/src/main/java/com/three_stack/digital_compass/backend/InvalidInputException.java
+++ backend/src/main/java/com/three_stack/digital_compass/backend/InvalidInputException.java
@@ -1,11 +1,14 @@
 package com.three_stack.digital_compass.backend;
 
-public class InvalidInputException extends 	Exception {
+public class InvalidInputException extends Exception {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = -5121994118845234438L;
+	
+	private Code code;
+	
+	public static enum Code {
+		INPUT_REJECTED, ILLEGAL_STATE
+	}
 	
 	public InvalidInputException() {
 		super();
@@ -18,13 +21,7 @@ public class InvalidInputException extends 	Exception {
 	public InvalidInputException(Code code, String message) {
 		super(message);
 		this.code = code;
-	}
-	
-	public static enum Code {
-		INPUT_REJECTED
-	}
-	
-	private Code code;
+	}	
 	
 	public Code getCode() {
 		return code;
